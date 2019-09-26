7e9290fecc0b3f6b9a68dbe61df0bff3931d2b0c
aspiringguru <bmatthewtaylor@gmail.com>
2019-09-26 23:36:07 +0000

    end part2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express--__',  title2: 'title2' });
 });
 
 module.exports = router;
