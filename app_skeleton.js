1088a57817f297cc3670f25386588284a4a87e5e
aspiringguru <bmatthewtaylor@gmail.com>
2019-09-27 00:19:04 +0000

    define local library scheme

--- app_skeleton.js
+++ app_skeleton.js
@@ -9,6 +9,16 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Set up mongoose connection
+var mongoose = require('mongoose');
+var dev_db_url = 'mongodb://127.0.0.1/library_skeleton'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
